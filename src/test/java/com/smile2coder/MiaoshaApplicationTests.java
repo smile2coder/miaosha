409ac099f34b72b3a1a6def9107e7ae07fb00040
smile2coder <406547968@qq.com>
2020-12-17 00:51:12 +0800

    init project

---
+++ src/test/java/com/smile2coder/MiaoshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile2coder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoshaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
